0cfa3a636657ab08bf5f9651bf3ef1263bef062e
JC Vizcarra <jvizcar@emory.edu>
2024-06-17 15:09:23 -0400

    refactoring app

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,30 +1,6 @@
 from girder_client import GirderClient
-from os import getenv
 from pathlib import Path
 import numpy as np
-
-
-def get_current_user():
-    """Get the current user based on the local API token."""
-    gc = get_gc()
-
-    # Get the user currently authenticated.
-    token_info = gc.get("token/current")
-    user = ""
-
-    for user_info in token_info["access"]["users"]:
-        user = gc.getUser(user_info["id"])["login"]
-        return gc, user
-
-    return gc, None
-
-
-def get_gc():
-    """Return an authenticated girder client."""
-    gc = GirderClient(apiUrl=getenv("DSA_API_URL"))
-    gc.authenticate(apiKey=getenv("DSA_API_TOKEN"))
-
-    return gc
 
 
 def create_new_task(project_id: str, task_name: str) -> str:
