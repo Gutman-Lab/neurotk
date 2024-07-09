80f128b4c6f2051b3db5e96e466033ccba6a1e2f
JC Vizcarra <jvizcar@emory.edu>
2024-07-09 16:27:19 -0400

    working on cli task

--- app/components/cli_panel.py
+++ app/components/cli_panel.py
@@ -1,4 +1,69 @@
-from dash import html
+from dash import html, dcc, callback, Output, Input, State, no_update
 import dash_bootstrap_components as dbc
+from pathlib import Path
+from utils import generate_cli_input_components
 
-cli_panel = html.Div("CLI")
+cli_panel = html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col(html.Div("CLI: "), width="auto"),
+                dbc.Col(dcc.Dropdown(options=[], id="cli-dropdown"), width=4),
+            ],
+            justify="start",
+        ),
+        dbc.Row(
+            [],
+            id="cli-inputs",
+            justify="start",
+            style={"marginTop": 10, "marginLeft": 5, "width": "75%"},
+        ),
+    ],
+    style={"marginLeft": 10},
+)
+
+
+# Callbacks
+@callback(
+    [
+        Output("cli-dropdown", "options"),
+        Output("cli-dropdown", "value"),
+    ],
+    Input("user-store", "storage_type"),
+)
+def start_cli_dropdown(_):
+    # Runs at the beginning of the app to load the available CLIs.
+    options = []
+
+    for fp in Path("cli-xmls").iterdir():
+        # Only check xml files.
+        if fp.suffix == ".xml":
+            # Add the CLI to the options.
+            cli_name = fp.stem
+
+            options.append({"label": fp.stem, "value": str(fp)})
+
+    return options, options[0]["value"] if len(options) else None
+
+
+@callback(
+    Output("cli-inputs", "children"),
+    Input("cli-dropdown", "value"),
+    prevent_initial_call=True,
+)
+def load_cli_inputs(cli_fp):
+    # Load the appropriate UI for the CLI selected.
+    # Read the XML file.
+    if len(cli_fp):
+        with open(cli_fp, "r") as fp:
+            xml_content = fp.read().strip()
+
+        components = generate_cli_input_components(xml_content)
+
+        return dbc.Container(
+            dbc.Card(
+                dbc.CardBody(components),
+            ),
+        )
+
+    return []
--- app/utils/generate_cli_input_components.py
+++ app/utils/generate_cli_input_components.py
@@ -4,241 +4,246 @@ import xml.etree.ElementTree as ET
 
 
 def generate_cli_input_components(
-    xml_string,
-    paramSetsToIgnore=("Frame and Style", "Dask", "Girder API URL and Key"),
-    disabled=False,
-    params=None,
-):
-    if params is None:
-        params = {}
+    xml_string: str,
+    # paramSetsToIgnore=("Frame and Style", "Dask", "Girder API URL and Key"),
+    # disabled=False,
+    # params=None,
+) -> list:
+    """Generate the CLI input components from the xml string.
 
+    Args:
+        xml_string (str): The xml string to parse.
+
+    Returns:
+        list: The list of dash components to render.
+
+    """
+    # if params is None:
+    #     params = {}
+
+    # Format the xml string.
     root = ET.fromstring(xml_string)
 
-    ## TO DO:  Hide anything related to DASK and Frame and Style--- this is not reevant
-
-    # There are certain parameters that should not be made directly visible in the UI such as a specific image
-    ## At least for now...
-    paramsToHide = []
-
-    components = []
+    components = [
+        html.H4("Inputs", className="card-title", style={"textAlign": "center"}),
+    ]
 
     for param in root.findall(".//parameters"):
-        param_components = []
-
-        label = param.find("label").text if param.find("label") is not None else ""
-
-        if label in paramSetsToIgnore:
+        if param.find("label") is not None and param.find("label").text != "IO":
             continue
 
-        param_components.append(html.H4("Inputs", className="card-title"))
+        for parameter in param:
+            tag = parameter.tag
 
-        ## This loops through all the various parameters, I want to hide image params for now..
-        hideImageParam = True
+            if tag == 'image'
+        # Loop through all the parameters as they appear in the xml image, not mater what type
 
-        if not hideImageParam:
-            for image in param.findall("image"):
-                name = image.find("name").text if image.find("name") is not None else ""
-                label_text = (
-                    image.find("label").text if image.find("label") is not None else ""
-                )
-                param_components.extend(
-                    [html.Label(label_text), dcc.Upload(id=name), html.Br()]
-                )
+    # for param in root.findall(".//parameters")
+    # for image in param.findall("image"):
+    #             name = image.find("name").text if image.find("name") is not None else ""
+    #             label_text = (
+    #                 image.find("label").text if image.find("label") is not None else ""
+    #             )
+    #             param_components.extend(
+    #                 [html.Label(label_text), dcc.Upload(id=name), html.Br()]
+    #             )
 
-        for region in param.findall("region"):
-            name = region.find("name").text if region.find("name") is not None else ""
-            label_text = (
-                region.find("label").text if region.find("label") is not None else ""
-            )
-            default = (
-                region.find("default").text
-                if region.find("default") is not None
-                else ""
-            )
-            param_components.extend(
-                [
-                    html.Label(label_text),
-                    dcc.Input(
-                        id={"type": "dynamic-input", "index": name},
-                        value=params.get(name, default),
-                        type="text",
-                        readOnly=False,
-                        disabled=disabled,
-                    ),
-                    html.Br(),
-                ]
-            )
+    #     for region in param.findall("region"):
+    #         name = region.find("name").text if region.find("name") is not None else ""
+    #         label_text = (
+    #             region.find("label").text if region.find("label") is not None else ""
+    #         )
+    #         default = (
+    #             region.find("default").text
+    #             if region.find("default") is not None
+    #             else ""
+    #         )
+    #         param_components.extend(
+    #             [
+    #                 html.Label(label_text),
+    #                 dcc.Input(
+    #                     id={"type": "dynamic-input", "index": name},
+    #                     value=params.get(name, default),
+    #                     type="text",
+    #                     readOnly=False,
+    #                     disabled=disabled,
+    #                 ),
+    #                 html.Br(),
+    #             ]
+    #         )
 
-        for region in param.findall("string"):
-            name = region.find("name").text if region.find("name") is not None else ""
-            label_text = (
-                region.find("label").text if region.find("label") is not None else ""
-            )
-            default = (
-                region.find("default").text
-                if region.find("default") is not None
-                else ""
-            )
-            param_components.extend(
-                [
-                    html.Label(label_text),
-                    dcc.Input(
-                        id={"type": "dynamic-input", "index": name},
-                        value=params.get(name, default),
-                        type="text",
-                        readOnly=False,
-                        disabled=disabled,
-                    ),
-                    html.Br(),
-                ]
-            )
+    #     for region in param.findall("string"):
+    #         name = region.find("name").text if region.find("name") is not None else ""
+    #         label_text = (
+    #             region.find("label").text if region.find("label") is not None else ""
+    #         )
+    #         default = (
+    #             region.find("default").text
+    #             if region.find("default") is not None
+    #             else ""
+    #         )
+    #         param_components.extend(
+    #             [
+    #                 html.Label(label_text),
+    #                 dcc.Input(
+    #                     id={"type": "dynamic-input", "index": name},
+    #                     value=params.get(name, default),
+    #                     type="text",
+    #                     readOnly=False,
+    #                     disabled=disabled,
+    #                 ),
+    #                 html.Br(),
+    #             ]
+    #         )
 
-        for enum in param.findall("string-enumeration"):
-            name = enum.find("name").text if enum.find("name") is not None else ""
-            label_text = (
-                enum.find("label").text if enum.find("label") is not None else ""
-            )
-            options = [elem.text for elem in enum.findall("element")]
+    #     for enum in param.findall("string-enumeration"):
+    #         name = enum.find("name").text if enum.find("name") is not None else ""
+    #         label_text = (
+    #             enum.find("label").text if enum.find("label") is not None else ""
+    #         )
+    #         options = [elem.text for elem in enum.findall("element")]
 
-            param_components.extend(
-                [
-                    html.Label(label_text),
-                    dcc.Dropdown(
-                        id={"type": "dynamic-input", "index": name},
-                        options=[{"label": op, "value": op} for op in options],
-                        value=params.get(name, options[0]),
-                        clearable=False,
-                        style={"maxWidth": 300},
-                        disabled=disabled,
-                    ),
-                    html.Br(),
-                ]
-            )
+    #         param_components.extend(
+    #             [
+    #                 html.Label(label_text),
+    #                 dcc.Dropdown(
+    #                     id={"type": "dynamic-input", "index": name},
+    #                     options=[{"label": op, "value": op} for op in options],
+    #                     value=params.get(name, options[0]),
+    #                     clearable=False,
+    #                     style={"maxWidth": 300},
+    #                     disabled=disabled,
+    #                 ),
+    #                 html.Br(),
+    #             ]
+    #         )
 
-        for vector in param.findall("double-vector"):
-            name = vector.find("name").text if vector.find("name") is not None else ""
-            label_text = (
-                vector.find("label").text if vector.find("label") is not None else ""
-            )
-            default = (
-                vector.find("default").text
-                if vector.find("default") is not None
-                else ""
-            )
-            param_components.extend(
-                [
-                    html.Label(label_text),
-                    dcc.Input(
-                        id=name,
-                        value=params.get(name, default),
-                        type="text",
-                        readOnly=False,
-                        disabled=disabled,
-                    ),
-                    html.Br(),
-                ]
-            )
-        for float_param in param.findall("float"):
-            name = (
-                float_param.find("name").text
-                if float_param.find("name") is not None
-                else ""
-            )
-            label_text = (
-                float_param.find("label").text
-                if float_param.find("label") is not None
-                else ""
-            )
-            default = (
-                float_param.find("default").text
-                if float_param.find("default") is not None
-                else ""
-            )
+    #     for vector in param.findall("double-vector"):
+    #         name = vector.find("name").text if vector.find("name") is not None else ""
+    #         label_text = (
+    #             vector.find("label").text if vector.find("label") is not None else ""
+    #         )
+    #         default = (
+    #             vector.find("default").text
+    #             if vector.find("default") is not None
+    #             else ""
+    #         )
+    #         param_components.extend(
+    #             [
+    #                 html.Label(label_text),
+    #                 dcc.Input(
+    #                     id=name,
+    #                     value=params.get(name, default),
+    #                     type="text",
+    #                     readOnly=False,
+    #                     disabled=disabled,
+    #                 ),
+    #                 html.Br(),
+    #             ]
+    #         )
+    #     for float_param in param.findall("float"):
+    #         name = (
+    #             float_param.find("name").text
+    #             if float_param.find("name") is not None
+    #             else ""
+    #         )
+    #         label_text = (
+    #             float_param.find("label").text
+    #             if float_param.find("label") is not None
+    #             else ""
+    #         )
+    #         default = (
+    #             float_param.find("default").text
+    #             if float_param.find("default") is not None
+    #             else ""
+    #         )
 
-            # Validate that the default value is a float
-            try:
-                float(default)
-            except ValueError:
-                default = 0
+    #         # Validate that the default value is a float
+    #         try:
+    #             float(default)
+    #         except ValueError:
+    #             default = 0
 
-            param_components.extend(
-                [
-                    html.Label(label_text, style={"marginRight": "10px"}),
-                    dcc.Input(
-                        id={"type": "dynamic-input", "index": name},
-                        value=params.get(name, float(default)),
-                        type="number",
-                        step=0.01,
-                        readOnly=False,
-                        style={
-                            "width": "60px",
-                            "textAlign": "right",
-                            "border": "1px solid #ccc",
-                            "marginRight": "5px",
-                            "margin": "2px 2px",  # vertical and horizontal margin
-                            "appearance": "number-input",  # for Firefox
-                            "MozAppearance": "number-input",  # for older Firefox versions
-                            "WebkitAppearance": "number-input",  # for Chrome and modern browsers
-                        },
-                        disabled=disabled,
-                    ),
-                    html.Br(),
-                ]
-            )
+    #         param_components.extend(
+    #             [
+    #                 html.Label(label_text, style={"marginRight": "10px"}),
+    #                 dcc.Input(
+    #                     id={"type": "dynamic-input", "index": name},
+    #                     value=params.get(name, float(default)),
+    #                     type="number",
+    #                     step=0.01,
+    #                     readOnly=False,
+    #                     style={
+    #                         "width": "60px",
+    #                         "textAlign": "right",
+    #                         "border": "1px solid #ccc",
+    #                         "marginRight": "5px",
+    #                         "margin": "2px 2px",  # vertical and horizontal margin
+    #                         "appearance": "number-input",  # for Firefox
+    #                         "MozAppearance": "number-input",  # for older Firefox versions
+    #                         "WebkitAppearance": "number-input",  # for Chrome and modern browsers
+    #                     },
+    #                     disabled=disabled,
+    #                 ),
+    #                 html.Br(),
+    #             ]
+    #         )
 
-        for int_param in param.findall("integer"):
-            name = (
-                int_param.find("name").text
-                if int_param.find("name") is not None
-                else ""
-            )
+    #     for int_param in param.findall("integer"):
+    #         name = (
+    #             int_param.find("name").text
+    #             if int_param.find("name") is not None
+    #             else ""
+    #         )
 
-            label_text = (
-                int_param.find("label").text
-                if int_param.find("label") is not None
-                else ""
-            )
+    #         label_text = (
+    #             int_param.find("label").text
+    #             if int_param.find("label") is not None
+    #             else ""
+    #         )
 
-            default = (
-                int_param.find("default").text
-                if int_param.find("default") is not None
-                else ""
-            )
+    #         default = (
+    #             int_param.find("default").text
+    #             if int_param.find("default") is not None
+    #             else ""
+    #         )
 
-            # Validate that the default value is a float
-            try:
-                int(default)
-            except ValueError:
-                default = 0
+    #         # Validate that the default value is a float
+    #         try:
+    #             int(default)
+    #         except ValueError:
+    #             default = 0
 
-            param_components.extend(
-                [
-                    html.Label(label_text, style={"marginRight": "10px"}),
-                    dcc.Input(
-                        id={"type": "dynamic-input", "index": name},
-                        value=params.get(name, int(default)),
-                        type="number",
-                        step=1,
-                        readOnly=False,
-                        style={
-                            "width": "60px",
-                            "textAlign": "right",
-                            "border": "1px solid #ccc",
-                            "marginRight": "5px",
-                            "margin": "2px 2px",  # vertical and horizontal margin
-                            "appearance": "number-input",  # for Firefox
-                            "MozAppearance": "number-input",  # for older Firefox versions
-                            "WebkitAppearance": "number-input",  # for Chrome and modern browsers
-                        },
-                        disabled=disabled,
-                    ),
-                    html.Br(),
-                ]
-            )
+    #         param_components.extend(
+    #             [
+    #                 html.Label(label_text, style={"marginRight": "10px"}),
+    #                 dcc.Input(
+    #                     id={"type": "dynamic-input", "index": name},
+    #                     value=params.get(name, int(default)),
+    #                     type="number",
+    #                     step=1,
+    #                     readOnly=False,
+    #                     style={
+    #                         "width": "60px",
+    #                         "textAlign": "right",
+    #                         "border": "1px solid #ccc",
+    #                         "marginRight": "5px",
+    #                         "margin": "2px 2px",  # vertical and horizontal margin
+    #                         "appearance": "number-input",  # for Firefox
+    #                         "MozAppearance": "number-input",  # for older Firefox versions
+    #                         "WebkitAppearance": "number-input",  # for Chrome and modern browsers
+    #                     },
+    #                     disabled=disabled,
+    #                 ),
+    #                 html.Br(),
+    #             ]
+    #         )
 
-        components.append(dbc.Card(dbc.CardBody(param_components), className="mb-3"))
+    #     components.append(dbc.Card(dbc.CardBody(param_components), className="mb-3"))
 
-    return dbc.Container(
-        components,
-        className="mt-3",
-    )
+    # return dbc.Container(
+    #     components,
+    #     className="mt-3",
+    # )
+
+    return components
